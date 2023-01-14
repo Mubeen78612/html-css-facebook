08feacda652a074cfc37efd0dbdbedab4af7e64f
mubeen <mubeenmalik>
2023-01-14 13:53:46 +0000

    first commit

---
+++ assets/form.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <div class="header">
+        <div class="form">
+       
+            <form>
+                <input type="text" placeholder="Name" name="name"><br>
+                <input type="password" placeholder="password" name="password"><br>
+                <button class="btn"> log in</button><br>
+                
+            </form>
+        </div>
+    </div>
+    
+</body>
+</html>
